c34c32580b6e9bd133e904c217eed4644b3fea42
feng <470883010@qq.com>
2021-06-02 15:12:10 +0800

    完成遍历城市功能

--- app/src/main/java/com/tsn/coolweather/util/HttpUtil.java
+++ app/src/main/java/com/tsn/coolweather/util/HttpUtil.java
@@ -6,7 +6,8 @@ import okhttp3.Request;
 public class HttpUtil {
     public static void sendOkHttpRequest(String address,okhttp3.Callback callback){
         OkHttpClient client=new OkHttpClient(); //实例化OkHttpClient
-        Request request=new Request.Builder() //创建一个请求对象
+        //创建一个请求对象
+        Request request=new Request.Builder()
                 .url(address)
                 .build();
         //开启一个异步请求
--- app/src/main/java/com/tsn/coolweather/util/Utility.java
+++ app/src/main/java/com/tsn/coolweather/util/Utility.java
@@ -2,6 +2,8 @@ package com.tsn.coolweather.util;
 
 import android.text.TextUtils;
 
+import com.tsn.coolweather.db.City;
+import com.tsn.coolweather.db.County;
 import com.tsn.coolweather.db.Province;
 
 import org.json.JSONArray;
@@ -10,7 +12,7 @@ import org.json.JSONObject;
 
 public class Utility {
     /**
-     * 解析和处理服务器返回的省级数据
+     * 解析和处理服务器返回的省级数据,这里将数据保存到数据库
      */
     public static boolean handleProvinceResponse(String response){
         if(!TextUtils.isEmpty(response)){
@@ -21,7 +23,7 @@ public class Utility {
                     Province province=new Province();
                     province.setProvinceName(provinceObject.getString("name"));
                     province.setProvinceCode(provinceObject.getInt("id"));
-                    province.save();
+                    province.save(); //数据库插入数据
                 }
                 return true;
             } catch (JSONException e) {
@@ -34,16 +36,17 @@ public class Utility {
     /**
      * 解析和处理服务器返回的市级数据
      */
-    public static boolean handleCityResponse(String response){
+    public static boolean handleCityResponse(String response,int provinceId){
         if(!TextUtils.isEmpty(response)){
             try{
-                JSONArray allProvinces=new JSONArray(response);
-                for(int i=0;i<allProvinces.length();i++){
-                    JSONObject provinceObject=allProvinces.getJSONObject(i);
-                    Province province=new Province();
-                    province.setProvinceName(provinceObject.getString("name"));
-                    province.setProvinceCode(provinceObject.getInt("id"));
-                    province.save();
+                JSONArray allCities=new JSONArray(response);
+                for(int i=0;i<allCities.length();i++){
+                    JSONObject cityObject=allCities.getJSONObject(i);
+                    City city=new City();
+                    city.setCityName(cityObject.getString("name"));
+                    city.setCityCode(cityObject.getInt("id"));
+                    city.setProvinceId(provinceId);
+                    city.save();
                 }
                 return true;
             } catch (JSONException e) {
@@ -56,16 +59,17 @@ public class Utility {
     /**
      * 解析和处理服务器返回的县级数据
      */
-    public static boolean handleCountyResponse(String response){
+    public static boolean handleCountyResponse(String response,int cityId){
         if(!TextUtils.isEmpty(response)){
             try{
-                JSONArray allProvinces=new JSONArray(response);
-                for(int i=0;i<allProvinces.length();i++){
-                    JSONObject provinceObject=allProvinces.getJSONObject(i);
-                    Province province=new Province();
-                    province.setProvinceName(provinceObject.getString("name"));
-                    province.setProvinceCode(provinceObject.getInt("id"));
-                    province.save();
+                JSONArray allCounties=new JSONArray(response);
+                for(int i=0;i<allCounties.length();i++){
+                    JSONObject countyObject=allCounties.getJSONObject(i);
+                    County county=new County();
+                    county.setCountyName(countyObject.getString("name"));
+                    county.setWeatherId(countyObject.getString("weather_id"));
+                    county.setCityId(cityId);
+                    county.save();
                 }
                 return true;
             } catch (JSONException e) {
